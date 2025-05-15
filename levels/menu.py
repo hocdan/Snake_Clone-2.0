7ba871f0c1c5a41ede0414abd150999b4417954a
hocdan <34260026+hocdan@users.noreply.github.com>
2025-05-15 01:59:48 -0300

    Update menu.py

--- levels/menu.py
+++ levels/menu.py
@@ -20,9 +20,9 @@ class Menu(State):
         #inicializando janela
         pyxel.mouse(True)
         #carregando icones do jogo
-        pyxel.load("/home/hacdan/Documents/Linux/Programacao/Python/Projetos/Pyxel/SNAKE_2.0/Assets/snake.pyxres")
+        pyxel.load("Assets/snake.pyxres")
         #carregando fonte das letras
-        self.fonte = pyxel.Font("/home/hacdan/Documents/Linux/Programacao/Python/Projetos/Pyxel/SNAKE_2.0/Assets/Fonts/VictoriaBold-8.bdf")
+        self.fonte = pyxel.Font("Assets/Fonts/VictoriaBold-8.bdf")
         #declarando tilemap para decoracao da tela tutorial
         self.decoration = world.World_SNAKE(pyxel.tilemaps[2], largura=16, altura=17, tamanho=8)
 
